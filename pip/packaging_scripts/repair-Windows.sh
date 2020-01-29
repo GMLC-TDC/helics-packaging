c8a12a3f04f6bb4ed0df6dbcc0710c49c923e1db
Ryan Mast <nightlark@users.noreply.github.com>
2020-01-29 13:52:18 -0800

    Use static linked ZeroMQ builds on Windows

--- pip/packaging_scripts/repair-Windows.sh
+++ pip/packaging_scripts/repair-Windows.sh
@@ -9,7 +9,7 @@ for whl in $PWD/wheelhouse/*.whl; do
   mkdir tmp_dir
   pushd tmp_dir
   wheel unpack "$whl"
-  cp ../helics/bin/libzmq*.dll helics-*/helics
+  #cp ../helics/bin/libzmq*.dll helics-*/helics
   wheel pack helics-*/ --dest-dir=../wheelhouse/fixed-wheels
   popd
   rm -rf tmp_dir
