83d667432945027848ca534871d0210a8774df39
Ryan Mast <mast9@llnl.gov>
2020-03-04 19:38:00 -0800

    Convert Windows style toolcache path to Unix style

--- pip/packaging_scripts/build-Windows.sh
+++ pip/packaging_scripts/build-Windows.sh
@@ -4,11 +4,14 @@ WINARCH=$1
 
 export CMAKE_PREFIX_PATH
 
+bash_runner_tool_cache=$(echo "/${RUNNER_TOOL_CACHE}" | sed -e 's/\\/\//g' -e 's/://')
+
 # Compile wheels
 pushd pip || exit $?
 OLD_PATH=$PATH
-for PYDIR in "${RUNNER_TOOL_CACHE}"/Python/3*/"${WINARCH}"; do
+for PYDIR in "${bash_runner_tool_cache}"/Python/3*/"${WINARCH}"; do
   pythonLocation="${PYDIR}"
+  export pythonLocation
   echo "Building wheel using Python at ${pythonLocation}"
   PATH="${PYDIR}/Scripts:${PYDIR}:$OLD_PATH"
   export PATH
@@ -19,8 +22,9 @@ done
 
 choco install -y swig || exit $?
 OLD_PATH="$HOME/swig-install/bin:$OLD_PATH"
-for PYDIR in "${RUNNER_TOOL_CACHE}"/Python/2*/"${WINARCH}"; do
+for PYDIR in "${bash_runner_tool_cache}"/Python/2*/"${WINARCH}"; do
   pythonLocation="${PYDIR}"
+  export pythonLocation
   echo "Building wheel using Python at ${pythonLocation}"
   PATH="${PYDIR}/Scripts:${PYDIR}:$OLD_PATH"
   export PATH
