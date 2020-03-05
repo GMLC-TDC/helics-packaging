fee3debc178c6e6e1d00d9e757ace12bd751e469
Ryan Mast <mast9@llnl.gov>
2020-03-04 19:22:28 -0800

    Export PATH with updated Python locations

--- pip/packaging_scripts/build-Windows.sh
+++ pip/packaging_scripts/build-Windows.sh
@@ -8,7 +8,10 @@ export CMAKE_PREFIX_PATH
 pushd pip || exit $?
 OLD_PATH=$PATH
 for PYDIR in "${RUNNER_TOOL_CACHE}"/Python/3*/"${WINARCH}"; do
+  pythonLocation="${PYDIR}"
+  echo "Building wheel using Python at ${pythonLocation}"
   PATH="${PYDIR}/Scripts:${PYDIR}:$OLD_PATH"
+  export PATH
   "$PYDIR/python" -m pip install --upgrade pip
   "$PYDIR/python" -m pip install setuptools wheel
   "$PYDIR/python" setup.py bdist_wheel --dist-dir=../wheelhouse
@@ -17,7 +20,10 @@ done
 choco install -y swig || exit $?
 OLD_PATH="$HOME/swig-install/bin:$OLD_PATH"
 for PYDIR in "${RUNNER_TOOL_CACHE}"/Python/2*/"${WINARCH}"; do
+  pythonLocation="${PYDIR}"
+  echo "Building wheel using Python at ${pythonLocation}"
   PATH="${PYDIR}/Scripts:${PYDIR}:$OLD_PATH"
+  export PATH
   "$PYDIR/python" -m pip install --upgrade pip
   "$PYDIR/python" -m pip install setuptools wheel
   "$PYDIR/python" setup.py bdist_wheel --dist-dir=../wheelhouse
