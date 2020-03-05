a6ff23a25dc9492409d452475c390203c1f1c07e
Ryan Mast <mast9@llnl.gov>
2020-03-04 19:07:44 -0800

    Combine Windows pip package builds into a single step

--- pip/packaging_scripts/build-Windows.sh
+++ pip/packaging_scripts/build-Windows.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
+WINARCH=$1
+
 export CMAKE_PREFIX_PATH
 
-python -m pip install --upgrade pip
-python -m pip install setuptools wheel
-
 # Compile wheels
-cd pip
-python setup.py bdist_wheel --dist-dir=../wheelhouse
+pushd pip || exit $?
+OLD_PATH=$PATH
+for PYDIR in "${RUNNER_TOOL_CACHE}"/Python/3*/"${WINARCH}"; do
+  PATH="${PYDIR}/Scripts:${PYDIR}:$OLD_PATH"
+  python -m pip install --upgrade pip
+  python -m pip install setuptools wheel
+  python setup.py bdist_wheel --dist-dir=../wheelhouse
+done
 
+choco install -y swig || exit $?
+OLD_PATH="$HOME/swig-install/bin:$OLD_PATH"
+for PYDIR in "${RUNNER_TOOL_CACHE}"/Python/2*/"${WINARCH}"; do
+  PATH="${PYDIR}/Scripts:${PYDIR}:$OLD_PATH"
+  python -m pip install --upgrade pip
+  python -m pip install setuptools wheel
+  python setup.py bdist_wheel --dist-dir=../wheelhouse
+done
+popd || exit $?
