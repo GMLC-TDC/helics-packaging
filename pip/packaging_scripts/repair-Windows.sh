189e35a52be0599b91a7b8fa89633394760c5ce1
Ryan Mast <mast9@llnl.gov>
2020-03-04 20:02:46 -0800

    Install wheel before repairing the wheels on Windows

--- pip/packaging_scripts/repair-Windows.sh
+++ pip/packaging_scripts/repair-Windows.sh
@@ -9,13 +9,17 @@ for whl in wheelhouse/*.post*.post.*.whl; do
     mv "$whl" "${whl//\.post\./.}"
 done
 
+# Install wheel module for fixing up the wheels
+python -m pip install --upgrade pip
+python -m pip install wheel
+
 # Bundle external shared libraries into the wheels
 for whl in $PWD/wheelhouse/*.whl; do
   mkdir tmp_dir
   pushd tmp_dir
-  python -m wheel unpack "$whl"
+  wheel unpack "$whl"
   #cp ../helics/bin/libzmq*.dll helics-*/helics
-  python -m wheel pack helics-*/ --dest-dir=../wheelhouse/fixed-wheels
+  wheel pack helics-*/ --dest-dir=../wheelhouse/fixed-wheels
   popd
   rm -rf tmp_dir
 done
