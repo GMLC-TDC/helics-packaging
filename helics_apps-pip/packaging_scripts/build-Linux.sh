a3444c6d5d1cc09b920cba531b1b538cbbd24aab
Ryan Mast <mast9@llnl.gov>
2020-03-06 21:48:14 -0800

    Build helics_apps pip package without a container

--- helics_apps-pip/packaging_scripts/build-Linux.sh
+++ helics_apps-pip/packaging_scripts/build-Linux.sh
@@ -2,6 +2,6 @@
 
 # Create the wheel
 pushd helics_apps-pip || exit $?
-/opt/python/cp37-cp37m/bin/python setup.py bdist_wheel --dist-dir=../wheelhouse --verbose
+python setup.py bdist_wheel --dist-dir=../wheelhouse --verbose
 popd || exit $?
 
--- helics_apps-pip/packaging_scripts/setup-Linux.sh
+++ helics_apps-pip/packaging_scripts/setup-Linux.sh
@@ -9,10 +9,10 @@ tar xzf Helics-*.tar.gz && rm Helics-*.tar.gz && mv Helics-* helics || exit $?
 # Add a copy of HELICS binaries to the data folder
 cp helics/bin/helics_* helics_apps-pip/helics_apps/data/bin/
 
-/opt/python/cp37-cp37m/bin/python -m pip install setuptools --upgrade
-whoami
-ls -lh helics_apps-pip/helics_apps/data/bin
-
 # Add lib64 to LD_LIBRARY_PATH so auditwheel can fix up the bdist wheel
 LD_LIBRARY_PATH="$PWD/helics/lib64:$LD_LIBRARY_PATH"
 export LD_LIBRARY_PATH
+
+# Make sure pip and required tools are set up
+pip install --upgrade pip
+pip install setuptools wheel auditwheel
