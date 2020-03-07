e925ae0897f68078efce3fef7e917a0e2ae2f2f8
Ryan Mast <mast9@llnl.gov>
2020-03-06 20:52:29 -0800

    Fix build step

--- helics_apps-pip/packaging_scripts/build-Linux.sh
+++ helics_apps-pip/packaging_scripts/build-Linux.sh
@@ -2,6 +2,6 @@
 
 # Compile wheel with a recent version of python
 pushd helics_apps-pip || exit $?
-/opt/cp37/bin/python setup.py bdist_wheel --dist-dir=../wheelhouse
+/opt/python/cp37/bin/python setup.py bdist_wheel --dist-dir=../wheelhouse
 popd || exit $?
 
