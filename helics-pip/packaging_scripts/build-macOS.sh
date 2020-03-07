de2e4555d847e3261ead070289d75e9c410769fc
Ryan Mast <mast9@llnl.gov>
2020-03-07 11:03:10 -0800

    Disable Python 2.7 macOS builds

--- helics-pip/packaging_scripts/build-macOS.sh
+++ helics-pip/packaging_scripts/build-macOS.sh
@@ -2,6 +2,8 @@
 
 export CMAKE_PREFIX_PATH
 
+# Disable SWIG for these builds
+mv /usr/local/bin/swig /usr/local/bin/swig-bak || true
 pushd helics-pip || exit $?
 OLD_PATH=$PATH
 for PYDIR in "${RUNNER_TOOL_CACHE}"/Python/3*/x64; do
@@ -15,8 +17,13 @@ for PYDIR in "${RUNNER_TOOL_CACHE}"/Python/3*/x64; do
   python setup.py bdist_wheel --dist-dir=../wheelhouse
 done
 
-# Swig is already installed on macOS
-brew install swig
+# Disable building the 2.7 interface for now
+# HELICS 2.4.1 has a bug that prevents building Python interfaces with SWIG
+# And Python 2.7 interface builds require swig
+exit 0
+
+# Swig is already installed on macOS, reenable it
+mv /usr/local/bin/swig-bak /usr/local/bin/swig
 for PYDIR in "${RUNNER_TOOL_CACHE}"/Python/2*/x64; do
   pythonLocation="${PYDIR}"
   export pythonLocation
