097d9581d76caa74475ab2cb82713a497a7b6c9d
Ryan Mast <mast9@llnl.gov>
2020-03-07 09:46:46 -0800

    Remove fetching the shared libs on Windows

--- helics_apps-pip/packaging_scripts/setup-Windows.sh
+++ helics_apps-pip/packaging_scripts/setup-Windows.sh
@@ -3,10 +3,6 @@
 HELICS_VERSION=$1
 WINARCH=$2
 
-# Install HELICS shared library for Windows
-curl -O -L "https://github.com/GMLC-TDC/HELICS/releases/download/v${HELICS_VERSION}/Helics-shared-${HELICS_VERSION}-${WINARCH}.tar.gz"
-tar xzf Helics-*.tar.gz && rm Helics-*.tar.gz && mv Helics-* helics || exit $?
-
 # Get HELICS apps for Windows
 curl -O -L "https://github.com/GMLC-TDC/HELICS/releases/download/v${HELICS_VERSION}/Helics-${HELICS_VERSION}-${WINARCH}.zip"
 unzip Helics-*.zip && rm Helics-*.zip && mv Helics-* helics || exit $?
