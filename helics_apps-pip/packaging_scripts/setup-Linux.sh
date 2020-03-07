e066d224c54c2a7206e3f68fbaf20213364e4093
Ryan Mast <mast9@llnl.gov>
2020-03-06 21:14:36 -0800

    Print some helics_apps pip debug info

--- helics_apps-pip/packaging_scripts/setup-Linux.sh
+++ helics_apps-pip/packaging_scripts/setup-Linux.sh
@@ -7,8 +7,13 @@ curl -O -L "https://github.com/GMLC-TDC/HELICS/releases/download/v${HELICS_VERSI
 tar xzf Helics-*.tar.gz && rm Helics-*.tar.gz && mv Helics-* helics || exit $?
 
 # Add a copy of HELICS binaries to the data folder
-mkdir -p helics_apps-pip/helics_apps/data/bin
 cp helics/bin/helics_* helics_apps-pip/helics_apps/data/bin/
+
+echo "helics subfolder:"
+ls helics
+
+echo "helics_apps-pip/helics_apps/data/bin subfolder:"
+ls helics_apps-pip/helics_apps/data/bin
 
 # Add lib64 to LD_LIBRARY_PATH so auditwheel can fix up the bdist wheel
 LD_LIBRARY_PATH="$PWD/helics/lib64:$LD_LIBRARY_PATH"
