0571b1a6a497e84a391935f51ede9b0c1817d5cb
Ryan Mast <mast9@llnl.gov>
2020-03-06 20:31:57 -0800

    Add helics_apps pip package

---
+++ helics-apps-pip/helics_apps/__init__.py
@@ -0,0 +1,30 @@
+import os
+import subprocess
+import sys
+
+from ._version import get_versions
+
+__version__ = get_versions()['version']
+del get_versions
+
+DATA = os.path.join(os.path.dirname(__file__), 'data')
+
+BIN_DIR = os.path.join(DATA, 'bin')
+
+def _program(name, args):
+    return subprocess.call([os.path.join(BIN_DIR, name)] + args)
+
+def helics_app():
+    raise SystemExit(_program('helics_app', sys.argv[1:]))
+
+def helics_broker():
+    raise SystemExit(_program('helics_broker', sys.argv[1:]))
+
+def helics_broker_server():
+    raise SystemExit(_program('helics_broker_server', sys.argv[1:]))
+
+def helics_player():
+    raise SystemExit(_program('helics_player', sys.argv[1:]))
+
+def helics_recorder():
+    raise SystemExit(_program('helics_recorder', sys.argv[1:]))
---
+++ helics-apps-pip/setup.py
@@ -0,0 +1,72 @@
+import io
+import os
+import versioneer
+
+from setuptools import setup
+
+# Get the directory setup.py is located in
+setup_py_dir = os.path.abspath(os.path.dirname(__file__))
+
+# Get the README.md contents
+README_contents = io.open(os.path.join(setup_py_dir, 'README.md'), encoding="utf-8").read()
+
+# Use versioneer to get the version tag
+PKG_VERSION = versioneer.get_version()
+
+# Try getting a version from PKG-INFO if this is a build from an sdist package
+if PKG_VERSION == "0+unknown":
+    try:
+        with open(os.path.join(setup_py_dir, 'PKG-INFO')) as f:
+            for line in f:
+                if line.startswith("Version:"):
+                    PKG_VERSION=line[8:].strip()
+    except IOError:
+        print("Unable to get a package version, using 0+unknown")
+
+setup(
+    name='helics_apps',
+    version=PKG_VERSION,
+    author='GMLC-TDC',
+    author_email='helicsdevelopers@helics.org',
+    maintainer='',
+    maintainer_email='',
+    url="https://github.com/GMLC-TDC/HELICS",
+    download_url="https://github.com/GMLC-TDC/HELICS/releases",
+    license='BSD',
+    keywords='helics co-simulation',
+    description='Hierarchical Engine for Large-scale Infrastructure Co-Simulation (HELICS)',
+    long_description=README_contents,
+    long_description_content_type='text/markdown',
+    packages=['helics_apps'],
+    package_data={'helics_apps': ['data/*']},
+    cmdclass=versioneer.get_cmdclass(),
+    entry_points={
+        'console_scripts': [
+            'helics_app=helics_apps:helics_app',
+            'helics_broker=helics_apps:helics_broker',
+            'helics_broker_server=helics_apps:helics_broker_server',
+            'helics_player=helics_apps:helics_player',
+            'helics_recorder=helics_apps:helics_recorder',
+        ]
+    },
+    zip_safe=False,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: Unix",
+        "Programming Language :: C",
+        "Programming Language :: C++",
+        "Programming Language :: Python",
+        "Topic :: Software Development",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Utilities",
+    ],
+)
