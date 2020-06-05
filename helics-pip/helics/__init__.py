f1bd309a1eef5f781aca4af35c5e845f68b8339a
Ryan Mast <mast9@llnl.gov>
2020-06-04 18:45:45 -0700

    Add suggestion to install latest VC++ Runtime for DLL load failed ImportError

--- helics-pip/helics/__init__.py
+++ helics-pip/helics/__init__.py
@@ -1,4 +1,17 @@
-from .helics import *
+import platform
+
+if platform.system() != "Windows":
+    from .helics import *
+else:
+    try:
+        from .helics import *
+    except ImportError as e:
+        # Python 3 has exception chaining: raise ImportError("Custom message") from e
+        # If Python 2 support is dropped, use that instead.
+        if 'DLL load failed' in str(e):
+            raise ImportError(str(e) + "\nSUGGESTED FIX: Install the latest Visual C++ Redistributable for Visual Studio 2019. See https://support.microsoft.com/en-us/help/2977003/the-latest-supported-visual-c-downloads for links.")
+        else:
+            raise ImportError(e)
 
 from ._version import get_versions
 __version__ = get_versions()['version']
